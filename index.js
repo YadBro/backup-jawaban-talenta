a009f22e94486b371b610afb25f5b46f49cf2814
Yadi Apriyadi <ncada708@gmail.com>
2023-01-10 10:11:34 +0700

    update

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const bodyParser = require('body-parser');
 const router = require('./src/routes/index.js');
 
 const app = express();
-app.use(cors());
+const corsOption = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOption));
 app.use(express.json());
-app.use(bodyParser.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/v1', router);
 
